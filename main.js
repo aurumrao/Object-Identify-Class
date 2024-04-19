c6e7aba9fd92baf31ca5b108f7d1072a96a1d12e
aurumrao <109160817+aurumrao@users.noreply.github.com>
2024-04-19 14:28:52 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+img = "";
+
+function preload(){
+    img = loadImage("dog_cat.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(640, 420);
+    canvas.center();
+}
+
+function draw(){
+    image(img, 0, 0, 640, 420);
+    fill("#FF2222");
+    text("Dog", 45, 75);
+    noFill();
+    stroke("FF2222");
+    rect(30, 60, 450, 350);
+}
