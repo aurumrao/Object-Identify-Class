c239519887bb429be4f994ef1f2d0bc258cb94b0
aurumrao <109160817+aurumrao@users.noreply.github.com>
2024-05-04 14:14:08 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 img = "";
+status = "";
 
 function preload(){
     img = loadImage("dog_cat.jpg");
@@ -7,15 +8,35 @@ function preload(){
 function setup(){
     canvas = createCanvas(640, 420);
     canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
 }
 
 function draw(){
     image(img, 0, 0, 640, 420);
     fill("#FF2222");
     text("Dog", 45, 75);
-    text("Cat", 395, 75);
     noFill();
     stroke("#FF2222");
     rect(30, 60, 450, 350);
 
+    fill("#FF2222");
+    text("Cat", 320, 120);
+    noFill();
+    stroke("#FF2222");
+    rect(300, 90, 270, 320);
+}
+
+function modelLoaded(){
+    console.log("Cocossd Loaded");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+    }
 }
