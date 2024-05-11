b49adac971ed60707a81987633fff8c3de30cd5f
aurumrao <109160817+aurumrao@users.noreply.github.com>
2024-05-11 14:14:46 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -7,23 +7,35 @@ function preload(){
 }
 
 function setup(){
-    canvas = createCanvas(640, 420);
-    canvas.position(500, 600)
+    canvas = createCanvas(380, 380);
+    canvas.position(500, 600);
+    video = createCapture(VIDEO);
+    video.size(380, 380);
+    video.hide();
+}
+function start(){
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
 }
 
 function draw(){
-    image(img, 0, 0, 640, 420);
+    image(video, 0, 0, 380, 380);
     if (status != ""){
+        objectDetector.detect(video, gotResult);
         for (i =0; i <objects.length; i++)
         {
-            document.getElementById("status").innerHTML = "Status: Object Detected";
+            r = random(255);
+            g = random(255);
+            b = random(255);
 
-            fill("#FF0000");
+            document.getElementById("status").innerHTML = "Status: Object Detected";
+            document.getElementById("object_count").innerHTML = objects.length + " Objects Detected";
+            fill(r, g, b);
+            
             percent = floor(objects[i].confidence * 100);
             text(objects[i].label + ", " + percent + "% Confident", objects[i].x + 15,  objects[i].y + 15);
             noFill();
-            stroke("#FF0000");
+            stroke(r, g, b);
             rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
         }
     }
@@ -32,7 +44,6 @@ function draw(){
 function modelLoaded(){
     console.log("Cocossd Loaded");
     status = true;
-    objectDetector.detect(img, gotResult);
 }
 
 function gotResult(error, results){
