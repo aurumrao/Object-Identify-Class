71d33be43a55cdaa1f82b8ce231ab003c41a1c37
aurumrao <109160817+aurumrao@users.noreply.github.com>
2024-05-11 14:20:16 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function preload(){
 
 function setup(){
     canvas = createCanvas(380, 380);
-    canvas.position(500, 300);
+    canvas.center();
     video = createCapture(VIDEO);
     video.size(380, 380);
     video.hide();
